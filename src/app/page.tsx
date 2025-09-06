c352971e18fafbbaa17cadb4dfd24135de8ad1f9
Ying Tong <yingtong0106@gmail.com>
2025-09-06 16:47:26 +0800

    feat: Add basic projects page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Intro from "../components/Intro";
+import Projects from "../components/Projects.tsx";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="h-screen flex flex-col w-full items-center justify-center mx-auto">
+      <main className="min-h-screen flex flex-col w-full items-center justify-center mx-auto">
         <Intro />
+        <Projects />
       </main>
       <Footer />
     </>
