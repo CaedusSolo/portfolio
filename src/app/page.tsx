640f166a6a8e88f45c7a3f2b68bd4da374175f74
Ying Tong <yingtong0106@gmail.com>
2025-09-06 15:20:52 +0800

    feat: Add basic Intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Intro from "../components/Intro";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="min-h-screen flex flex-col w-full items-center justify-center mx-auto">
-        <h1 className="text-4xl">Hello, I'm Ying Tong</h1>
-        <h3 className="text-lg">A developer passionate in web dev</h3>
-        <Image src="/next.svg" width={180} height={200} alt="NextJS logo" />
+      <main className="h-screen flex flex-col w-full items-center justify-center mx-auto">
+        <Intro />
       </main>
       <Footer />
     </>
