0ba6276cdfcf4aa4009addb30f6e4809bb175a71
Ying Tong <yingtong0106@gmail.com>
2025-09-24 11:40:45 +0800

    fix: Update resume nav link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -172,7 +172,7 @@ export default function Navbar() {
               Contact
             </Link>
             <a
-              href="https://google.com"
+              href="/Resume.pdf"
               target="_blank"
               rel="noreferrer"
               className="block rounded-md px-3 py-2 text-gray-300 hover:text-white hover:bg-white/10 transition"
